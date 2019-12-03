eb92038c91805459f833f2a4f2bf118cc36c2859
kskillen07 <kerrylouiseskillen@gmail.com>
2019-12-03 21:35:54 +0000

    Tested YouTube placement.

---
+++ js/youtube.js
@@ -0,0 +1,16 @@
+var CHANNEL_ID = "UCupvZG-5ko_eiXAupbDfxWw";
+var API_KEY = "AIzaSyBYo-POffSRcVnNr6WL0ZNUpJNjS37ZY0s";
+
+$.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search?key="+API_KEY+"&channelId="+CHANNEL_ID+"&part=snippet,id&order=date&maxResults=1",
+    context: document.body
+}).done(function(response) {
+    var items = response.items;
+    if(items.length>0) {
+        var item = items[0];
+        var videoid = "http://www.youtube.com/embed/"+item.id;
+        console.log("Latest ID: '"+videoid+"'");
+        var video = "<iframe width='420' height='315' src='"+videoid+"' frameborder='0' allowfullscreen></iframe>"; 
+        $('#static_video').html(video);
+    }
+});
