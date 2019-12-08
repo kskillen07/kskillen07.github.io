668b5833b1e64731f1a54803b0427f76f4f850df
kskillen07 <kerrylouiseskillen@gmail.com>
2019-12-08 22:19:56 +0000

    Header img optimise

--- js/youtube.js
+++ js/youtube.js
@@ -12,8 +12,8 @@ $.ajax({
     if(items.length>0) {
         var item = items[0];
         var videoid = "https://www.youtube.com/embed/"+item.id.videoId;
-        console.log("Latest ID: '"+videoid+"'");
-        var video = "<iframe width='420' height='315' src='"+videoid+"' frameborder='0' allowfullscreen></iframe>"; 
+        //console.log("Latest ID: '"+videoid+"'");
+        var video = "<iframe width='370' height='220' src='"+videoid+"' frameborder='0' allowfullscreen></iframe>"; 
         $('#static_video').html(video);
     }
 });
